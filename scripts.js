ee220a498bb2477a3652cec5a1e23fde95c9f38a
clarabrancalhao <brancalhaoclara@gmail.com>
2020-06-25 20:45:11 -0300

    done!

---
+++ scripts.js
@@ -0,0 +1,25 @@
+const modalOverlay = document.querySelector('.modalOverlay');
+const cards = document.querySelectorAll('.food');
+const foodInfos = document.querySelector('.food-infos');
+
+for (let card of cards) {
+  card.addEventListener("click", function(){
+    const imgId = card.id
+    const foodName = card.querySelector('h1').textContent
+    const criadoPor = card.querySelector('p').textContent
+    modalOverlay.classList.add("active");
+
+    const modalImg = foodInfos.querySelector('img');
+    modalImg.src = `/assets/${imgId}.png`
+
+    foodInfos.querySelector('h1').textContent = foodName
+    foodInfos.querySelector('p').textContent = criadoPor
+    
+  })
+}
+
+document.querySelector(".closeModal").addEventListener("click", function() {
+  modalOverlay.classList.remove("active")
+})
+
+
